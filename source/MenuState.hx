7dc8a1cd9bcf8258f36360a3252b6bc7eac3faa4
IdealistCat <mattallan.h@gmail.com>
2024-12-22 15:16:09 -0600

    remove assetpaths and menu state

---
+++ source/MenuState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class MenuState extends FlxState
+{
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
