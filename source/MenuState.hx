b3fa9d4610fc26af5ff706732fd535dde23af2dd
IdealistCat <mattallan.h@gmail.com>
2024-12-22 15:21:15 -0600

    Typing (kinda)

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,16 +1,40 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
+
+using StringTools;
 
 class MenuState extends FlxState
 {
+	var text:FlxText = new FlxText(0, 0, 0, "", 16);
+
 	override function create()
 	{
+		add(text);
 		super.create();
 	}
 
 	override function update(elapsed:Float)
 	{
+		if (FlxG.keys.justPressed.ANY)
+		{
+			if (!FlxG.keys.justReleased.ESCAPE)
+			{
+				for (key in FlxG.keys.getIsDown())
+				{
+					updateText(text.text + key);
+				}
+			}
+		}
+
 		super.update(elapsed);
+	}
+
+	function updateText(setText:String)
+	{
+		text.text = setText;
+		text.screenCenter();
 	}
 }
